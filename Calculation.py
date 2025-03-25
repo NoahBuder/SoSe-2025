fab9a4875b7ad5b66a06c1ee6dcea8bd37feec1e
JustinBundschuh <bj9049@mci4me.at>
2025-03-25 11:34:44 +0100

    Update Calculation.py

--- Calculation.py
+++ Calculation.py
@@ -1,7 +1,7 @@
 import my_functions  # Ohne .py
 
 if __name__ == "__main__":
-    person  = my_functions.build_person("Noah","Buder", "Male", 20)
+    person  = my_functions.build_person("Bundschuh","Justin", "Male", 21)
     experiment = my_functions.build_experiment("Test","21.03.2025", "Alex", "running")
     print(person)
     print(experiment)
